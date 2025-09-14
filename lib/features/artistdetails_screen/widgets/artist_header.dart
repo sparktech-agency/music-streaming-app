cbe84a3485a6d9c6a7e81619c5533b4247c5ea3f
arif-youknow <inbox.arifkhan@gmail.com>
2025-09-15 04:59:16 +0600

    follow section done

--- lib/features/artistdetails_screen/widgets/artist_header.dart
+++ lib/features/artistdetails_screen/widgets/artist_header.dart
@@ -62,24 +62,79 @@ class ArtistHeader extends StatelessWidget {
             '1600000 Listeners',
             style: TextStyle(color: Colors.white),
           ),
-          SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              IconButton(
-                onPressed: () {},
-                icon: Icon(Icons.favorite, color: Colors.white),
-              ),
-              IconButton(
-                onPressed: () {},
-                icon: Icon(Icons.share, color: Colors.white),
-              ),
-              IconButton(
-                onPressed: () {},
-                icon: Icon(Icons.play_arrow, color: Colors.white),
-              ),
-            ],
+          SizedBox(height: 40),
+
+          //follow section start
+          Container(
+            height: 64.0,
+            padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+            decoration: BoxDecoration(
+              color: Color(0xff312B36),
+              borderRadius: BorderRadius.circular(12.0),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+
+                      },
+                      child: Container(
+                        padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 8.0),
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [
+                              AppColors.secondaryColor,
+                              AppColors.primaryColor,
+                              ],
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+
+                          ),
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        child: Text('Follow', style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                        ),),
+
+                    ),
+                    ),
+                    SizedBox(width: 15.0),
+                    SvgPicture.asset(
+                      'assets/app_icons/share.svg',
+                      width: 20,
+                      height: 20,
+                    )
+                    //share icon button
+                  ],
+                ),
+                Row(
+                  children: [
+                    SvgPicture.asset(
+                      'assets/app_icons/shuffle.svg',
+                      width: 24,
+                      height: 24,
+                    ),
+                    SizedBox(width: 15.0),
+                    SvgPicture.asset(
+                      'assets/app_icons/play_circle.svg',
+                      width: 40,
+                      height: 40,
+                    ),
+                  ],
+                ),
+
+              ],
+
+            )
+
           ),
+          //follow section end
+
         ],
       ),
     );
